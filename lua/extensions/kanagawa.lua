6e9836e4aa9c8036e27b61545c47d018084e3181
Emmanuel S. Emokor <emokores256@gmail.com>
2026-01-07 16:39:12 +0300

    Enable kanagawa transparency

--- lua/extensions/kanagawa.lua
+++ lua/extensions/kanagawa.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("kanagawa").setup({
 			compile = true,
-			-- transparent = true,
+			transparent = true,
 			overrides = function(colors)
 				return {
 					["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)

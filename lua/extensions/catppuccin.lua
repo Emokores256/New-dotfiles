9b7a85c804543ac50a9c9353d00feab8a7b39e32
Emmanuel S. Emokor <emokores256@gmail.com>
2026-01-07 16:38:53 +0300

    New plugin
    Catppuccin colorscheme

---
+++ lua/extensions/catppuccin.lua
@@ -0,0 +1,22 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true, -- disables setting the background color.
+			float = {
+				transparent = true, -- enable transparent floating windows
+				solid = true, -- use solid styling for floating windows, see |winborder|
+			},
+			auto_integrations = true,
+		})
+		-- setup must be called before loading
+		-- vim.cmd.colorscheme("catppuccin")
+	end,
+}

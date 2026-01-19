02a8de4d792c80ab31f66f58b73b381d311d5fa8
Emmanuel S. Emokor <emokores256@gmail.com>
2026-01-19 11:37:18 +0300

    Lazy to use material for UI

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,8 @@ require("lazy").setup({
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "kanagawa" } },
+	-- install = { colorscheme = { "kanagawa" } },
+	install = { colorscheme = { "material" } },
 	-- install = { colorscheme = { "darcula-dark" } },
 	-- install = { colorscheme = { "catppuccin-mocha" } },
 

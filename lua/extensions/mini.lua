b6c8b2defcfd04a0e9b72e9d91612841a1fa6f41
Emmanuel S. Emokor <emokores256@gmail.com>
2026-01-19 11:36:31 +0300

    Config for statusline
    Optional

--- lua/extensions/mini.lua
+++ lua/extensions/mini.lua
@@ -24,8 +24,46 @@ return {
 		--  You could remove this setup call if you don't like it,
 		--  and try some other statusline plugin
 		local statusline = require("mini.statusline")
-		-- set use_icons to true if you have a Nerd Font
-		statusline.setup({ use_icons = vim.g.have_nerd_font })
+		statusline.setup({
+			-- set use_icons to true if you have a Nerd Font
+			use_icons = vim.g.have_nerd_font,
+
+			-- Rewrite and reconfig for mini.statusline to match material theme.
+			-- -- NOTE: This might not be necessary, since there's a "mini" declaration in the plugins listing.
+			-- vim.api.nvim_create_autocmd("ColorScheme", {
+			-- 	pattern = "material",
+			-- 	callback = function()
+			-- 		local material = require("material.colors")
+			--
+			-- 		vim.api.nvim_set_hl(0, "MiniStatuslineMode", {
+			-- 			fg = material.editor.bg,
+			-- 			bg = material.main.blue,
+			-- 			bold = true,
+			-- 		})
+			--
+			-- 		vim.api.nvim_set_hl(
+			-- 			0,
+			-- 			"MiniStatuslineDevinfo",
+			-- 			{ fg = material.editor.fg, bg = material.main.cyan }
+			-- 		)
+			-- 		vim.api.nvim_set_hl(
+			-- 			0,
+			-- 			"MiniStatuslineFilename",
+			-- 			{ fg = material.editor.fg, bg = material.main.green }
+			-- 		)
+			-- 		vim.api.nvim_set_hl(
+			-- 			0,
+			-- 			"MiniStatuslineFileinfo",
+			-- 			{ fg = material.editor.fg, bg = material.main.yellow }
+			-- 		)
+			-- 		vim.api.nvim_set_hl(
+			-- 			0,
+			-- 			"MiniStatuslineInactive",
+			-- 			{ fg = material.editor.fg_dark, bg = material.editor.bg_alt }
+			-- 		)
+			-- 	end,
+			-- }),
+		})
 
 		-- You can configure sections in the statusline by overriding their
 		-- default behavior. For example, here we set the section for
